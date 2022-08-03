91186d0c2c8d067fd26d091152db07193a4b6669
vargas0912 <vargas0912@gmail.com>
2022-08-02 21:17:21 -0500

    SideMenu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
 
@@ -7,7 +9,7 @@ import { SidemenuComponent } from './sidemenu/sidemenu.component';
 @NgModule({
   imports: [
     CommonModule, 
-    
+    RouterModule
   ],
   declarations: [SidemenuComponent],
   exports: [
--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,14 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem
+{
+  texto: string;
+  ruta: string;
+}
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html'
 })
-export class SidemenuComponent implements OnInit {
+export class SidemenuComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  templateMenu: MenuItem[]= [
+    {
+      texto: 'Basicos',
+      ruta: './template/basicos'
+    },
+    {
+      texto: 'Dinamicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches'
+    },
+  ]
+  reactiveMenu: MenuItem[]= [
+    {
+      texto: 'Basicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      texto: 'Dinamicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './reactive/switches'
+    },
+  ]
 
 }

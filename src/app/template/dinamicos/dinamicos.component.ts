4bef6e108f3ac9d82e22ffb219ed744cb940caf8
vargas0912 <vargas0912@gmail.com>
2022-08-15 22:22:14 -0500

    Add dynamic form

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html'
 })
 export class DinamicosComponent implements OnInit {
+  @ViewChild('myForm') myForm!: NgForm;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  nameValid(): boolean {
+    return this.myForm?.controls['nombre']?.invalid &&
+      this.myForm?.controls['nombre']?.touched;
+  }
+
+  guardar(myForm: NgForm)
+  {
+    console.log("salvado");
+    
   }
 
 }

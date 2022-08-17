074e9264b854942be50b39389d1a7137a4bc6019
vargas0912 <vargas0912@gmail.com>
2022-08-16 19:44:44 -0500

    Add dynamic elements

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html'
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
   @ViewChild('myForm') myForm!: NgForm;
 
-  constructor() { }
-
-  ngOnInit() {
+  persona: Persona = {
+    nombre: 'Gerardo',
+    favoritos: [
+      {id: 1, nombre: 'Guitar hero'},
+      {id: 2, nombre: 'Metal gear'}
+    ]
   }
-
 
   nameValid(): boolean {
     return this.myForm?.controls['nombre']?.invalid &&
@@ -23,6 +35,10 @@ export class DinamicosComponent implements OnInit {
   {
     console.log("salvado");
     
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index, 1);
   }
 
 }

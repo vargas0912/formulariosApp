47833f29caad4788f5ac7ca3ae822288298deec9
vargas0912 <vargas0912@gmail.com>
2022-08-16 22:03:35 -0500

    Finish switches

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html'
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  persona = {
+    genre: 'F',
+    notificaciones: true
   }
+
+  terminos: boolean = false;
 
 }

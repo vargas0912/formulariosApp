e202ab7c76ce0518670b6a25a1155dbc7a1dc5a5
vargas0912 <vargas0912@gmail.com>
2022-08-17 21:48:19 -0500

    Finish task

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html'
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent  {
 
-  constructor() { }
+  myForm: FormGroup = this.formBuilder.group({
+    nombre:       [, [Validators.required, Validators.minLength(3)]]
+  });
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder ) { }
+
+  nameValid(campo: string) {
+    return this.myForm.controls?.[campo]?.errors 
+      && this.myForm.controls[campo]?.touched;
+  }
+
+  save(){
+    if (this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
   }
 
 }
